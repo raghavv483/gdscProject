2c59c16eab46e1f19bf437fe8ee6488e2773ff7d
RAGHAV KHANDELWAL <154792988+raghavv483@users.noreply.github.com>
2024-07-30 15:28:33 +0530

    signUp.js

--- routes/signUp.js
+++ routes/signUp.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();  
 
 
+const app = express();  
+app.set("view engine", "ejs");  
+app.set("views", path.join(__dirname, "views"));  
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
 
-router.post("/signup",(req,res)=>{
-    let{name,email,password}=req.body;
-    const user=user
+router.route("/"),get.((req,res)=>{
+   res.render("signUp.ejs")
 })
 
 
