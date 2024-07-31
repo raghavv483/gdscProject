603edbb4030671c6645e638470b293a221ce01aa
RAGHAV KHANDELWAL <154792988+raghavv483@users.noreply.github.com>
2024-07-31 13:13:22 +0530

    multer.middleware.js

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -1,4 +1,4 @@
-import multer from "multer";
+const multer=require("multer")
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -9,6 +9,8 @@ const storage = multer.diskStorage({
     }
   })
 
-export const upload = multer({ 
-    storage,
-})
+const upload = multer({ 
+    storage
+});
+
+module.exports=upload;
