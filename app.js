5782d991f15920071257be9c6cd6eead6371dc10
RAGHAV KHANDELWAL <154792988+raghavv483@users.noreply.github.com>
2024-07-31 13:15:36 +0530

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express=require("express");
 const app=express();
+const path=require("path");
+require('./routes/projectRoutes');
 
 
+app.set("view engine", "ejs");  
+app.set("views", path.join(__dirname, "views"));  
+app.use(express.json());  
+app.use(express.urlencoded({ extended: true }));  
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.use("/",require("./routes/projectRoutes"));
-app.use("/",require("./routes/loginRoute"));
 
 
 
